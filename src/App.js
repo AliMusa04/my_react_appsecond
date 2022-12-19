e188241843b3cd58ea5cb52100d53be74b33465a
AliMusa04 <tu74nx176@code.edu.az>
2022-12-19 19:33:21 +0400

    my changes in class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Welcome from './components/Welcome'
+
+const users = [{name:'Ali',surname:'Musayev',age:'19',id:'1',src:'https://cdn.britannica.com/05/94905-050-1830515C/Whirlpool-Galaxy-NGC-5195-Sc.jpg'},
+{name:'Kenan',surname:'Akbarli',age:'18',id:'2',src:'https://cdn.britannica.com/05/94905-050-1830515C/Whirlpool-Galaxy-NGC-5195-Sc.jpg'},
+{name:'Ferid',surname:'Qara',age:'20',id:'3',src:"https://cdn.britannica.com/05/94905-050-1830515C/Whirlpool-Galaxy-NGC-5195-Sc.jpg"}]
 
 function App() {
   return (
-    <h1>Hello world1234</h1>
+
+    <>
+    {users.map((user)=>{
+      return <Welcome id={user.id}name={user.name} surname={user.surname} age={user.age} img={user.src} />
+    })}
+    <Welcome/>
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <h3>salam456</h3>
+    </div>
+  )
+}
+
+export default Footer
